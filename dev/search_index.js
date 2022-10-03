8b4014c689aa8f3d54335eb876224690ac634833
Documenter.jl <documenter@juliadocs.github.io>
2022-10-03 13:56:58 +0000

    build based on 852c10a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TrackingFloats","category":"page"},{"location":"#TrackingFloats","page":"Home","title":"TrackingFloats","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TrackingFloats.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TrackingFloats]","category":"page"}]
+}
